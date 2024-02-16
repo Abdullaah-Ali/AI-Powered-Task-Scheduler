8695d6502fc1030beca0b79583a8f4f09d179b37
Abdullaah-Ali <134156961+Abdullaah-Ali@users.noreply.github.com>
2024-02-16 18:12:05 +0500

    added the schema for the user task

--- routes/task.js
+++ routes/task.js
@@ -31,13 +31,58 @@ const taskschema = new mongoose.Schema({
 
 const UserTasks = mongoose.model('UserTasks', taskschema);
 
-router.get('/', authenticateToken,  (req, res) => {
+
+router.route('/')
+.get(authenticateToken, async (req, res) => {
+    try {
         res.render(path.join(__dirname, 'template', 'task.ejs'));
-    
-    
-}); 
-    
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+.post(authenticateToken, async (req, res) => {
+    try {
+        const { priority, project_name, description, due_date } = req.body;
+        const userEmail = req.user.email; // Extract user email from JWT payload
 
+        const existingTask = await UserTasks.findOne({ userEmail });
 
+        if (existingTask) {
+            // Task exists, update the task
+            const updateFields = {};
+
+            // Check which fields are present in the request body and update only those fields
+            if (project_name) updateFields.projectName = project_name;
+            if (description) updateFields.description = description;
+            if (priority) updateFields.priority = priority;
+            if (due_date) updateFields.duedate = due_date;
+
+            const updatedTask = await UserTasks.findOneAndUpdate(
+                { userEmail },
+                updateFields,
+                { new: true }
+            );
+
+            res.json(updatedTask);
+        } else {
+            // Task does not exist, create a new task
+            const newTask = new UserTasks({
+                projectName: project_name,
+                priority: priority,
+                description: description,
+                duedate: due_date,
+                userEmail: userEmail // Associate the task with the user's email
+            });
+
+            const savedTask = await newTask.save();
+            res.json(savedTask);
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 module.exports = router;
